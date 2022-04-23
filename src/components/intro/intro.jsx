6ff39b1e435ac8fbfa1bac8e555f099bb879c96a
NY4TI <tanim.iqbal13@gmail.com>
2022-04-23 02:31:32 -0400

    intro

---
+++ src/components/intro/intro.jsx
@@ -0,0 +1,10 @@
+const Intro = () => {
+    return (
+        <div className="i">
+            <div className="i-left">left</div>
+            <div className="i-right">right</div>
+        </div>
+    )
+}
+
+export default Intro
