b9b30c6757ae3fd3f55d8e8ff8f1d6f312819586
NY4TI <tanim.iqbal13@gmail.com>
2022-04-23 02:55:17 -0400

    Title

--- src/components/intro/intro.jsx
+++ src/components/intro/intro.jsx
@@ -1,7 +1,21 @@
+import "./intro.css"
+
 const Intro = () => {
     return (
         <div className="i">
-            <div className="i-left">left</div>
+            <div className="i-left">
+                <div className="i-left-wrapper">
+                    <h2 className="i-intro">Hello, My name is</h2>
+                    <h1 className="i-name">Tanim Iqbal</h1>
+                    <i className="i-title">
+                        <div className="i-title-wrapper">
+                            <div className="i-title-item">Web Developer</div>
+                            <div className="i-title-item">Photographer</div>
+                            <div className="i-title-item">UI/UX Designer</div>
+                        </div>
+                    </i>
+                </div>
+            </div>
             <div className="i-right">right</div>
         </div>
     )
