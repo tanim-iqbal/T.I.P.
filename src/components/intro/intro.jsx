fb6ae58c12ea40b6897301b6792ae69164fd2e24
NY4TI <tanim.iqbal13@gmail.com>
2022-04-24 17:03:59 -0400

    Test BG

--- src/components/intro/intro.jsx
+++ src/components/intro/intro.jsx
@@ -15,6 +15,7 @@ const Intro = () => {
                             <div className="i-title-item">Photographer</div>
                         </div>
                     </div>
+                    <br></br>
                     <div className="i-desc">
                       I am an aspiring web developer looking to hone my newly aquired skills by working with a team or company that is in need of a highly motivated and eager to learn individual. 
                     </div>
