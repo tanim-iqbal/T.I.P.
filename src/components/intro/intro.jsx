8e3abb7b8bb9c6bc99fe2b47ad75ac99748a2e18
NY4TI <tanim.iqbal13@gmail.com>
2022-04-23 21:17:14 -0400

    Animation

--- src/components/intro/intro.jsx
+++ src/components/intro/intro.jsx
@@ -7,13 +7,13 @@ const Intro = () => {
                 <div className="i-left-wrapper">
                     <h2 className="i-intro">Hello, My name is</h2>
                     <h1 className="i-name">Tanim Iqbal</h1>
-                    <i className="i-title">
+                    <div className="i-title">
                         <div className="i-title-wrapper">
                             <div className="i-title-item">Web Developer</div>
-                            <div className="i-title-item">Photographer</div>
                             <div className="i-title-item">UI/UX Designer</div>
+                            <div className="i-title-item">Photographer</div>
                         </div>
-                    </i>
+                    </div>
                 </div>
             </div>
             <div className="i-right">right</div>
