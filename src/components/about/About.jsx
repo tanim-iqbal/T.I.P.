f7aaf53cba80041caa8c8424d430aa0b1a26b0f8
NY4TI <tanim.iqbal13@gmail.com>
2022-04-25 13:39:44 -0400

    Start of second component

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -3,7 +3,11 @@ import "./about.css"
 const About = () => {
     return (
         <div className="a">
-about
+            <div className="a-left">
+                <div className="a-card"></div>
+                <div className="a-card"></div>
+            </div>
+            <div className="a-right"></div>
         </div>
     )
 }
