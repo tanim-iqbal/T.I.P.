a0c063f660aa24acb098e3d771859930c820852c
NY4TI <tanim.iqbal13@gmail.com>
2022-05-05 15:48:11 -0400

    intro

--- src/components/intro/intro.jsx
+++ src/components/intro/intro.jsx
@@ -7,7 +7,7 @@ const Intro = () => {
             <div className="i-left">
                 <div className="i-left-wrapper">
                     <h2 className="i-intro">Hello, My name is</h2>
-                    <h1 className="i-name">Tanim I.</h1>
+                    <h1 className="i-name">Tanim Iqbal</h1>
                     <div className="i-title">
                         <div className="i-title-wrapper">
                             <div className="i-title-item">Web Developer</div>
