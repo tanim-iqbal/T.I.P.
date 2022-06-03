99cf140d78333edf1cc83307ee5b3a21e7abf04e
NY4TI <tanim.iqbal13@gmail.com>
2022-06-03 16:11:58 -0400

    about

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -2,7 +2,7 @@ import "./about.css"
 
 const About = () => {
     return (
-        <div className="a">ABOUT
+        <div className="a">This is the about section which needs to be finished.
             <div className="a-left">
                 <div className="a-card"></div>
                 <div className="a-card"></div>
